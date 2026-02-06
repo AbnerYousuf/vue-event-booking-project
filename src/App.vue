b2ac2dbdd4ddae7bdedd3b0924d3abdede9416e4
AbnerYousuf <yousufabner@gmail.com>
2026-02-06 15:17:05 -0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <main class="container mx-auto my-1 space-y-1">
     <h1 class="text-3xl text-center text-teal-800 font-semibold">Abner's Event Booking Application!</h1>
-    <h2 class="text-xl text-center text-teal-800 underline">Event List</h2>
+    <h2 class="text-xl text-center text-teal-800 underline">All Available Events</h2>
+    <h2 class="text-xl text-center text-teal-800 underline">Your Booked Events</h2>
   </main>
 </template>
