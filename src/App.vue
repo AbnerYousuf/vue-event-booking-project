be51dfe92aada488517713f0d5d151d2b272033c
AbnerYousuf <yousufabner@gmail.com>
2026-02-06 15:15:04 -0700

    changed aesthetics, added headers
    made the page teal-themed and added a header and subheader

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
-  <main>
-    hello
+  <main class="container mx-auto my-1 space-y-1">
+    <h1 class="text-3xl text-center text-teal-800 font-semibold">Abner's Event Booking Application!</h1>
+    <h2 class="text-xl text-center text-teal-800 underline">Event List</h2>
   </main>
 </template>
