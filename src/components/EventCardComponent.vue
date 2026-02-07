083569d747373158fff5b520f0594802fe745ab5
AbnerYousuf <yousufabner@gmail.com>
2026-02-06 21:28:46 -0700

    Update EventCardComponent.vue
    updated the card layout for aesthetics + added fields

--- src/components/EventCardComponent.vue
+++ src/components/EventCardComponent.vue
@@ -1,6 +1,7 @@
 <template>
-    <div class="bg-white rounded-lg shadow-md p-4">
-        <h3 class="text-lg font-semibold text-teal-800">Event Placeholder</h3>
-        <p class="text-gray-600">This is a placeholder for an event card component.</p>
+    <div class="bg-teal-50 border-teal-950 rounded-lg p-2">
+        <h3 class="text-lg font-semibold text-teal-800 p-2 border-b border-teal-100">Event Title</h3>
+        <p class="text-teal-700 p-2 border-b border-teal-100">Event Time</p>
+        <p class="text-teal-700 p-2">Event Description</p>
     </div>
 </template>
