c840189c74ec25a113d4c8faa6690a277d99355b
AbnerYousuf <yousufabner@gmail.com>
2026-02-06 22:09:49 -0700

    fixed spacing + added commenting
    im tired so i might sleep for now. more tomorrow

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,29 @@
 <template>
+
   <main class="container mx-auto space-x-auto my-4 space-y-4">
+    
     <h1 class="text-3xl text-center text-teal-800 font-semibold">Abner's Event Booking Application!</h1>
+    
     <h2 class="text-xl text-center text-teal-800 underline">All Available Events</h2>
+    
     <section class="grid grid-cols-2 gap-4">
-      <EventCardComponent v-for="i in 8" :key="i" title="Internship/Job 2026" time="2026-08-01" description="Please let this happen!"/>
+
+      <EventCardComponent v-for="i in 8" :key="i" 
+      title="Internship/Job 2026"
+      time="2026-08-01"
+      description="Please let this happen!"
+      @register="console.log('Register Event Emitted!')"
+      @dropout="console.log('Dropout Event Emitted!')"
+      />
+      <!-- dummy data for 8 event cards -->
     </section>
+    
     <h2 class="text-xl text-center text-teal-800 underline">Your Booked Events</h2>
+    <!-- section to display user's booked events, currently empty bc its unimplemented -->
   </main>
 </template>
 
 <script setup>
   import EventCardComponent from '@/components/EventCardComponent.vue';
+  //importing the EventCardComponent to be used in this parent component
 </script>
--- src/components/EventCardComponent.vue
+++ src/components/EventCardComponent.vue
@@ -1,9 +1,23 @@
 <template>
+
     <div class="bg-teal-50 border-teal-950 rounded-lg p-2">
+
         <h3 class="text-lg font-semibold text-teal-800 p-2 border-b border-teal-100">{{ title }}</h3>
         <p class="text-teal-700 p-2 border-b border-teal-100">{{ time }}</p>
-        <p class="text-teal-700 p-2">{{ description }}</p>
+        <p class="text-teal-700 p-2 border-b border-teal-100">{{ description }}</p>
+        <!-- display event information passed from parent component via props -->
+
+        <section class="flex justify-end gap-2 p-2">
+            <button @click="$emit('register')" class="bg-teal-200 text-white px-4 py-2 rounded hover:bg-teal-400">
+                Register
+            </button>
+            <button @click="$emit('dropout')" class="bg-red-200 text-white px-4 py-2 rounded hover:bg-red-400">
+                Drop Out
+            </button>
+        </section>
+        <!-- buttons to trigger registration/deregistration events emitted to parent component -->
     </div>
+
 </template>
 
 <script setup>
@@ -12,4 +26,8 @@
         time: String,
         description: String
     });
+    //props to handle event information passed from parent component
+
+    defineEmits(['register','dropout'])
+    //events to handle event registration/deregistration
 </script>
