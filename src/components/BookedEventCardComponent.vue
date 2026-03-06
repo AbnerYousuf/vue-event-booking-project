0c425d3642563231d97efadc801761630c5b620e
AbnerYousuf <yousufabner@gmail.com>
2026-03-06 16:01:13 -0700

    fixed booked event formatting + prepared to use a dummy json server for testing purposes

--- src/components/BookedEventCardComponent.vue
+++ src/components/BookedEventCardComponent.vue
@@ -2,13 +2,13 @@
 
     <SectionedCardComponent>
 
-        <h3 class="text-lg font-semibold text-teal-800 p-2">{{ title }}</h3>
-        <!-- display event information passed from parent component via props -->
-
         <section class="flex justify-between gap-2 p-2">
+            <h3 class="text-lg font-semibold text-teal-800 p-2">{{ title }}</h3>
+            <!-- display event information passed from parent component via props -->
+
             <ButtonComponent @clickevent="$emit('dropout')" eventtext="Drop Out" variant="red" />
+            <!-- button to trigger deregistration event emitted to parent component -->
         </section>
-        <!-- buttons to trigger registration/deregistration events emitted to parent component -->
     </SectionedCardComponent>
 
 </template>
